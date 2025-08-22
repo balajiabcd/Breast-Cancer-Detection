b5024988f3427478e1d38d88c220495bf6cac761
Balaji <68639299+balajiabcd@users.noreply.github.com>
2025-08-22 15:09:53 +0200

    changes
    some error corrections

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,11 @@
 import unittest
-from src.data_utils import load_data, preprocess_data
-from src.model import train_and_evaluate
+from data_utils import load_data, preprocess_data
+from model import train_and_evaluate
 from sklearn.linear_model import LogisticRegression
 
 class TestModel(unittest.TestCase):
     def test_pipeline_runs(self):
-        df = load_data("data/breast-cancer.csv)
+        df = load_data("data/breast-cancer.csv")
         X_train, X_test, y_train, y_test = preprocess_data(df)
         model = LogisticRegression(max_iter=1000)
         train_and_evaluate(model, X_train, X_test, y_train, y_test)
